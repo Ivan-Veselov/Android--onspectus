87fb7cda7f0a3c7d0993d8b0bd16137acf2191a0
tbelova <yukikomodo@gmail.com>
2016-10-09 17:54:39 +0300

    Yay

---
+++ 05-toasts/toast1.java
@@ -0,0 +1,8 @@
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    Toast.makeText(this, "[^_^]", Toast.LENGTH_LONG).show();
+}
+
